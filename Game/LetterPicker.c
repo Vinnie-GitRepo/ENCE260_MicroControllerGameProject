0b4717bda133c37c6903f033ea6ad2519c0f3fd4
Vinnie <vjj14@uclive.ac.nz>
2018-10-15 18:22:35 +1300

    Added a banner to LetterPicker.c and made more consistency adjustments.

--- Game/LetterPicker.c
+++ Game/LetterPicker.c
@@ -1,10 +1,10 @@
-/*
-# File:   Animations.c
-# Author: Group417
-# Date:   10 OCT 2018
-# Descr:  Animations on the LED board
-
-LetterPicker.o */
+/***********************************************************************
+# File:          LetterPicker.c
+# Group:         417
+# Authors:       Robert Condon, Vinnie Jamieson
+# Description:   Module for our assignment's text processing
+# Last Modified: 15 OCT 2018
+***********************************************************************/
 
 #include "LetterPicker.h"
 #include "system.h"
@@ -19,107 +19,122 @@ LetterPicker.o */
 #include "Starter.h"
 
 
-/* Displays the scores of wins and loses until navsiwtch is pushed*/
+/*
+ * Displays the scores of wins and loses until navsiwtch is pushed
+ */
 int DisplayScores(char Wins, char Loses)
 {
 
-	system_init();
+    system_init();
+    ScrollText_init();
 
+    char s[] = { ' ','W', Wins,' ','L', Loses};
 
-	ScrollText_init();
-	char s[] = { ' ','W', Wins,' ','L', Loses};
+    tinygl_text(s);
+    TCNT1 = 0;
+    while (1) {
 
-	tinygl_text(s);
-	TCNT1 = 0;
-	while(1) {
-		
-		tinygl_update();
-		navswitch_update ();
-		if (navswitch_push_event_p (NAVSWITCH_PUSH)) {
-		    break;
-		}
-	}
-	return 1;
+        tinygl_update();
+        navswitch_update();
+        if (navswitch_push_event_p(NAVSWITCH_PUSH)) {
+            break;
+        }
+    }
+    return 1;
 }
 
-/* Displays the Gold total until navsiwtch is pushed*/
+
+
+/*
+ * Displays the Gold total until navsiwtch is pushed
+ */
 int DisplayGold(char Gold)
 {
 
-	system_init();
+    system_init();
+    ScrollText_init();
 
+    char s[] = { ' ','G', 'O', 'L', 'D', ' ', Gold};
 
-	ScrollText_init();
-	char s[] = { ' ','G', 'O', 'L', 'D', ' ', Gold};
+    tinygl_text(s);
+    TCNT1 = 0;
+    while (1) {
 
-	tinygl_text(s);
-	TCNT1 = 0;
-	while(1) {
-		
-		tinygl_update();
-		navswitch_update ();
-		if (navswitch_push_event_p (NAVSWITCH_PUSH)) {
-		    break;
-		}
-	}
-	return 1;
+        tinygl_update();
+        navswitch_update();
+        if (navswitch_push_event_p(NAVSWITCH_PUSH)) {
+            break;
+        }
+    }
+
+    return 1;
 }
 
-/* Displays the scores 'YOU WON' or 'YOU LOST' until navsiwtch is pushed*/
+
+
+/*
+ * Displays the scores 'YOU WON' or 'YOU LOST' until navsiwtch is pushed
+ */
 int DisplayWinner(char Wins)
 {
 
-	system_init();
+    system_init();
+    ScrollText_init();
+    char* s;
 
-	char* s;
-	ScrollText_init();
-	if(Wins == '5') {
-		s = "YOU WON!";
-	} else {
-		s = "YOU LOST </3";
-	}
+    if (Wins == '5') {
+        s = "YOU WON!";
+    } else {
+        s = "YOU LOST </3";
+    }
 
-	tinygl_text(s);
-	TCNT1 = 0;
-	while(1) {
-		
-		tinygl_update();
-		navswitch_update ();
-		if (navswitch_push_event_p (NAVSWITCH_PUSH)) {
-		    break;
-		}
-	}
-	return 1;
+    tinygl_text(s);
+    TCNT1 = 0;
+
+    while (1) {
+
+        tinygl_update();
+        navswitch_update();
+        if (navswitch_push_event_p(NAVSWITCH_PUSH)) {
+            break;
+        }
+    }
+
+    return 1;
 }
 
-/** Displays winners of GTS and the type of victory/loss it was */
+/*
+ * Displays winners of GTS and the type of victory/loss it was
+ */
 int DisplayWinnerGTS(char Wins, char Gold, char OtherGold)
 {
 
-	system_init();
+    system_init();
 
-	char* s;
-	ScrollText_init();
-	if(Wins == 3) {
-		s = "YOU TRAPPED THEM YOU WIN";
-	} else if(Wins == -1){
-		s = "YOU GOT TRAPPED YOU LOSE";
-	} else if(Gold == 5) {
-		s = "YOU GOT 5 GOLD YOU WIN";
-	} else if(OtherGold == 5){
-		s = "THEY GOT 5 GOLD YOU LOSE";
-	}
+    char* s;
+    ScrollText_init();
+    if (Wins == 3) {
+        s = "YOU TRAPPED THEM YOU WIN";
+    } else if (Wins == -1){
+        s = "YOU GOT TRAPPED YOU LOSE";
+    } else if (Gold == 5) {
+        s = "YOU GOT 5 GOLD YOU WIN";
+    } else if (OtherGold == 5){
+        s = "THEY GOT 5 GOLD YOU LOSE";
+    }
 
-	tinygl_text(s);
-	TCNT1 = 0;
-	while(1) {
-		
-		
-		navswitch_update ();
-		tinygl_update();
-		if (navswitch_push_event_p (NAVSWITCH_PUSH)) {
-		    break;
-		}
-	}
-	return 1;
+    tinygl_text(s);
+    TCNT1 = 0;
+
+    while (1) {
+
+        navswitch_update();
+        tinygl_update();
+
+        if (navswitch_push_event_p(NAVSWITCH_PUSH)) {
+            break;
+        }
+    }
+
+    return 1;
 }
